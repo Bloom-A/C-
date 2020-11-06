023c14f5ef7513f2187d7c008a5e756f3af0d4f8
src <2014115640@qq.com>
2020-11-06 10:02:01 +0800

    实现登录菜单

---
+++ SwitchAdmin.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc,char *argv[]){
+	
+	system("cls");
+	printf("---交换机后台管理---\n");
+	printf("1 - 创建账号\n");
+	printf("2 - IP管理\n");
+	printf("3 - 端口管理\n");
+	printf("4 - 退出\n");
+	printf("请选择【1  --  4】");
+	
+	getchar();
+	return 0;
+}
